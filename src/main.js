612821ac7ea8512ca82bc6a480c5c4f47c1cd8fc
veenjenga <vionajoynjenga@gmail.com>
2025-07-15 17:24:32 +0300

    My vue app

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'//impeort the root component app from a single file component
+import './style.css'
+import App from './App.vue'
+import './index.css'
+
+
+createApp(App).mount('#app')// This is where the Vue app will be mounted
